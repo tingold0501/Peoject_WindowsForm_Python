82e7d5b4683765a5bd6458b025ff15df281fb2c0
Phạm Huỳnh Tín <116443704+PhamHuynhTin@users.noreply.github.com>
2023-02-19 22:40:30 +0700

    Update Code Form - Phạm Huỳnh Tín

--- the_shoes/core/mainform.py
+++ the_shoes/core/mainform.py
@@ -25,6 +25,12 @@ s.theme_use('clam')
 s.configure("red.Horizontal.TProgressbar", foreground='red', background='#4f4f4f')
 progress = Progressbar(mainForm, style="red.Horizontal.TProgressbar", orient=HORIZONTAL, length=1070, mode='determinate', )
 
+def register():
+    window_register = Tk()
+    window_register.title("Register Window")
+    window_register.geometry("1040x740")
+
+    window_register.mainloop()
 
 def login_window():
     window_login = Tk()
@@ -37,12 +43,33 @@ def login_window():
 
     frame_left_window.pack(side=LEFT)
 #///////////////////////////////////// WinDow Login LEFT
-    frame_right_window = Frame(window_login,width=520,height=740,background='#DFEBE9')
+    bg_color_window = '#DFEBE9'
+    frame_right_window = Frame(window_login,width=520,height=740,background=bg_color_window)
     lable_login = Label(frame_right_window, text='Login',background='#DFEBE9',foreground='#749BAE')
-    style1 = ('Calibri (Body)', 10)
+    style1 = ('Calibri (Body)', 25)
     lable_login.config(font=style1)
-    lable_login.place(x=150, y=200)
+    lable_login.place(x=130, y=180)
+
+    lable_create_new1 = Label(frame_right_window,text="Don't have an account ?",background=bg_color_window,foreground='#37677E')
+    style2 = ('Calibri (Body)', 12)
+    lable_create_new1.config(font=style2)
+    lable_create_new1.place(x=130, y=230)
+
+    btn_create_new = Button(frame_right_window, width=10, height=1, foreground='#EE5757', text='Create Now', command=register, border=0,bg=bg_color_window)
+    btn_create_new.config(font=style2)
+    btn_create_new.place(x=300,y=227)
+
+    lable_create_new1 = Label(frame_right_window, text="Email Or User Name", background=bg_color_window,foreground='#37677E',activebackground=bg_color_window)
+    style3 = ('Calibri (Body)', 8)
+    lable_create_new1.config(font=style3)
+    lable_create_new1.place(x=140, y=260)
+
+
+
+
     frame_right_window.pack(side=RIGHT)
+
+
 
     window_login.mainloop()
 def bar():
